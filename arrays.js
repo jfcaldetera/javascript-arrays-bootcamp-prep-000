7e7e8ecf65e35cc4b9f24c2f158547633ccd5059
Jhenna Foronda <jfcaldetera@gmail.com>
2018-06-25 17:40:36 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,43 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, toAdd)
+{
+  return var newArray = [toAdd, ...array]
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array, toAdd)
+{
+  return array.unshift(toAdd)
+}
+
+function addElementToEndOfArray(array, toAdd)
+{
+  return var newArray = [...array, toAdd]
+}
+
+function destructivelyAddElementToEndOfArray(array, toAdd)
+{
+  return array.push(toAdd)
+}
+
+function accessElementInArray(array, index)
+{
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array)
+{
+  return array.shift()
+}
+
+function removeElementFromBeginningOfArray(array)
+{
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array)
+{
+  array.pop()
+  return array
+}
