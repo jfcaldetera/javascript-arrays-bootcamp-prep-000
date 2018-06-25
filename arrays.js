da10e57c373b3c2622968ecb7a00a9dcb14adcd2
Jhenna Foronda <jfcaldetera@gmail.com>
2018-06-25 17:56:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, toAdd)
 {
-  return var newArray = [toAdd, ...array]
+  return [toAdd, ...array]
 
 }
 
