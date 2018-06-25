da3bae363e4dc63e1c57436214db9328c6f19d6e
Jhenna Foronda <jfcaldetera@gmail.com>
2018-06-25 18:00:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -43,3 +43,9 @@ function destructivelyRemoveElementFromEndOfArray(array)
   array.pop()
   return array
 }
+
+function removeElementFromEndOfArray(array)
+{
+  return array.slice(0, array.length - 1)
+}
+
